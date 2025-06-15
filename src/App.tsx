01bc21fff141c06857ad2693d8b64380b6570cc4
JeztC <dagreenrs@gmail.com>
2025-06-15 23:30:39 +0300

    feat: add files

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,33 @@ import {
     Routes, Route, Navigate
 } from "react-router-dom"
 import About from "./components/About";
-import Footer from "./components/Footer";
 import {ThemeProvider, Theme} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import {ThemeContext} from './themes/theme-context';
 import Education from "./components/Education";
 import Header from "./components/Header";
-import FloatingIcons from "./components/FloatingIcons";
 import Contact from "./components/Contact";
 import Links from "./components/Links";
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import './index.css'
 import {darkTheme, lightTheme} from "./themes/theme-context";
 import GithubCard from "./components/GithubCard";
+import Projects from "./components/Projects";
 
 const App = () => {
     const initialTheme = localStorage.getItem('mode') === 'light' ? lightTheme : darkTheme;
     const [mode, setMode] = React.useState<'light' | 'dark'>(localStorage.getItem('mode') as 'light' | 'dark');
     const [theme, setTheme] = React.useState<Theme>(initialTheme);
+    const newMode = mode === 'light' ? 'dark' : 'light';
+
+    React.useEffect(() => {
+        setTheme(newMode === 'light' ? darkTheme : lightTheme);
+    }, [mode]);
 
     const toggleMode = () => {
-        const newMode = mode === 'light' ? 'dark' : 'light';
         setMode(newMode);
         localStorage.setItem('mode', newMode);
     };
-
-    React.useEffect(() => {
-        const newMode = mode === 'light' ? 'dark' : 'light';
-        setTheme(newMode === 'light' ? darkTheme : lightTheme);
-    }, [mode]);
 
     return (
         <ThemeProvider theme={theme}>
@@ -53,14 +51,13 @@ const App = () => {
                                     <Route path="/education" element={<Education/>} />
                                     <Route path="/contact" element={<Contact/>} />
                                     <Route path="/links" element={<Links/>} />
+                                    <Route path="/projects" element={<Projects/>} />
                                     <Route path="/github" element={<GithubCard/>} />
                                     <Route path="/" element={<Navigate replace to="/about" />} />
                                 </Routes>
                             </CSSTransition>
                         </TransitionGroup>
-                        <Footer/>
                     </Fragment>
-                    <FloatingIcons />
                 </Router>
             </ThemeContext.Provider>
         </ThemeProvider>
