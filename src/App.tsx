ffc6ae73d0904fd579cd6e4c4608aa2aa9722b81
Jesse <dagreenrs@gmail.com>
2025-10-19 15:30:48 +0300

    feat: change default theme to system

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { ThemeProvider } from "@mui/material/styles";
 const App = () => {
     return (
         <>
-            <ThemeProvider theme={theme}>
+            <ThemeProvider theme={theme} defaultMode='system'>
                 <CssBaseline />
                 <Router>
                     <Fragment>
